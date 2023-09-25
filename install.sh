c82ab8c91a00f1427b7b6afefcfe430ba930f1ee
Restyled.io <commits@restyled.io>
2023-09-25 23:08:41 +0000

    Restyled by shfmt

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #print-banner
-figlet -f smblock -c "iemafzalhassan" 
-figlet -f smbraille -c "Custom Grub Boot loader Themes" 
-
+figlet -f smblock -c "iemafzalhassan"
+figlet -f smbraille -c "Custom Grub Boot loader Themes"
 
 #/**
-# *  
+# *
 # *
 # * @license MIT
 #
@@ -14,7 +13,7 @@ figlet -f smbraille -c "Custom Grub Boot loader Themes"
 #
 # * @version 1.0.0
 #
-# * @link  https://github.com/iemafzalhassan  
+# * @link  https://github.com/iemafzalhassan
 #
 # *
 # *
@@ -26,178 +25,189 @@ figlet -f smbraille -c "Custom Grub Boot loader Themes"
 THEME_DIR='/boot/grub/themes'
 THEME_NAME=''
 
-function echo_title() {     echo -ne "\033[1;44;37m${*}\033[0m\n"; }
-function echo_caption() {   echo -ne "\033[0;1;44m${*}\033[0m\n"; }
-function echo_bold() {      echo -ne "\033[0;1;34m${*}\033[0m\n"; }
-function echo_danger() {    echo -ne "\033[0;31m${*}\033[0m\n"; }
-function echo_success() {   echo -ne "\033[0;32m${*}\033[0m\n"; }
-function echo_warning() {   echo -ne "\033[0;33m${*}\033[0m\n"; }
+function echo_title() { echo -ne "\033[1;44;37m${*}\033[0m\n"; }
+function echo_caption() { echo -ne "\033[0;1;44m${*}\033[0m\n"; }
+function echo_bold() { echo -ne "\033[0;1;34m${*}\033[0m\n"; }
+function echo_danger() { echo -ne "\033[0;31m${*}\033[0m\n"; }
+function echo_success() { echo -ne "\033[0;32m${*}\033[0m\n"; }
+function echo_warning() { echo -ne "\033[0;33m${*}\033[0m\n"; }
 function echo_secondary() { echo -ne "\033[0;34m${*}\033[0m\n"; }
-function echo_info() {      echo -ne "\033[0;35m${*}\033[0m\n"; }
-function echo_primary() {   echo -ne "\033[0;36m${*}\033[0m\n"; }
-function echo_error() {     echo -ne "\033[0;1;31merror:\033[0;31m\t${*}\033[0m\n"; }
-function echo_label() {     echo -ne "\033[0;1;32m${*}:\033[0m\t"; }
-function echo_prompt() {    echo -ne "\033[0;36m${*}\033[0m "; }
+function echo_info() { echo -ne "\033[0;35m${*}\033[0m\n"; }
+function echo_primary() { echo -ne "\033[0;36m${*}\033[0m\n"; }
+function echo_error() { echo -ne "\033[0;1;31merror:\033[0;31m\t${*}\033[0m\n"; }
+function echo_label() { echo -ne "\033[0;1;32m${*}:\033[0m\t"; }
+function echo_prompt() { echo -ne "\033[0;36m${*}\033[0m "; }
 
 function splash() {
-    local hr
-    hr=" **$(printf "%${#1}s" | tr ' ' '*')** "
-    echo_title "$hr"
-    echo_title " * $1 * "
-    echo_title "$hr"
-    echo
+  local hr
+  hr=" **$(printf "%${#1}s" | tr ' ' '*')** "
+  echo_title "$hr"
+  echo_title " * $1 * "
+  echo_title "$hr"
+  echo
 }
 
 function check_root() {
-    # Checking for root access and proceed if it is present
-    ROOT_UID=0
-    if [[ ! "${UID}" -eq "${ROOT_UID}" ]]; then
-        # Error message
-        echo_error 'Run me as root.'
-        echo_info 'try sudo ./install.sh'
-        exit 1
-    fi
+  # Checking for root access and proceed if it is present
+  ROOT_UID=0
+  if [[ ! "${UID}" -eq "${ROOT_UID}" ]]; then
+    # Error message
+    echo_error 'Run me as root.'
+    echo_info 'try sudo ./install.sh'
+    exit 1
+  fi
 }
 
 function select_theme() {
-    themes=('CyberEXS' 'Cyberpunk' 'CyberRe' 'CyberSynchro' 'fallout' 'grub_linea' 'mr-robot' 'shodan' 'vimix' 'virtual_future' 'Quit')
+  themes=('CyberEXS' 'Cyberpunk' 'CyberRe' 'CyberSynchro' 'fallout' 'grub_linea' 'mr-robot' 'shodan' 'vimix' 'virtual_future' 'Quit')
 
-    PS3=$(echo_prompt '\nChoose The Theme You Want: ')
-    select THEME_NAME in "${themes[@]}"; do
-        case "$THEME_NAME" in
-            'CyberEXS')
-                splash 'Installing CyberEXS Theme...'
-                break;;
-            'Cyberpunk')
-                splash 'Installing Cyberpunk Theme...'
-                break;;
-            'CyberRe')
-                splash 'Installing CyberRe Theme...'
-                break;;
-            'CyberSynchro')
-                splash 'Installing CyberSynchro Theme...'
-                break;;
-            'fallout')
-                splash 'Installing fallout Theme...'
-                break;;
-            'grub_linea')
-                splash 'Installing grub_linea Theme...'
-                break;;
-            'mr-robot')
-                splash 'Installing mr-robot Theme...'
-                break;;
-            'shodan')
-                splash 'Installing shodan Theme...'
-                break;;
-            'vimix')
-                splash 'Installing vimix Theme...'
-                break;;
-             'virtual_future')
-                splash 'Installing virtual_future Theme...'
-                break;;
-            'Quit')
-                echo_info 'User requested exit...!'
-                exit 0;;
-            *) echo_warning "invalid option \"$REPLY\"";;
-        esac
-    done
+  PS3=$(echo_prompt '\nChoose The Theme You Want: ')
+  select THEME_NAME in "${themes[@]}"; do
+    case "$THEME_NAME" in
+      'CyberEXS')
+        splash 'Installing CyberEXS Theme...'
+        break
+        ;;
+      'Cyberpunk')
+        splash 'Installing Cyberpunk Theme...'
+        break
+        ;;
+      'CyberRe')
+        splash 'Installing CyberRe Theme...'
+        break
+        ;;
+      'CyberSynchro')
+        splash 'Installing CyberSynchro Theme...'
+        break
+        ;;
+      'fallout')
+        splash 'Installing fallout Theme...'
+        break
+        ;;
+      'grub_linea')
+        splash 'Installing grub_linea Theme...'
+        break
+        ;;
+      'mr-robot')
+        splash 'Installing mr-robot Theme...'
+        break
+        ;;
+      'shodan')
+        splash 'Installing shodan Theme...'
+        break
+        ;;
+      'vimix')
+        splash 'Installing vimix Theme...'
+        break
+        ;;
+      'virtual_future')
+        splash 'Installing virtual_future Theme...'
+        break
+        ;;
+      'Quit')
+        echo_info 'User requested exit...!'
+        exit 0
+        ;;
+      *) echo_warning "invalid option \"$REPLY\"" ;;
+    esac
+  done
 }
 
 function backup() {
-    # Backup grub config
-    echo_info 'cp -an /etc/default/grub /etc/default/grub.bak'
-    cp -an /etc/default/grub /etc/default/grub.bak
+  # Backup grub config
+  echo_info 'cp -an /etc/default/grub /etc/default/grub.bak'
+  cp -an /etc/default/grub /etc/default/grub.bak
 }
 
 function install_theme() {
-    # create themes directory if not exists
-    if [[ ! -d "${THEME_DIR}/${THEME_NAME}" ]]; then
-        # Copy theme
-        echo_primary "Installing $THEME_NAME theme..."
+  # create themes directory if not exists
+  if [[ ! -d "${THEME_DIR}/${THEME_NAME}" ]]; then
+    # Copy theme
+    echo_primary "Installing $THEME_NAME theme..."
 
-        echo_info "mkdir -p \"$THEME_DIR/$THEME_NAME\""
-        mkdir -p "$THEME_DIR/$THEME_NAME"
+    echo_info "mkdir -p \"$THEME_DIR/$THEME_NAME\""
+    mkdir -p "$THEME_DIR/$THEME_NAME"
 
-        echo_info "cp -a ./themes/\"$THEME_NAME\"/* \"$THEME_DIR/$THEME_NAME\""
-        cp -a ./themes/"$THEME_NAME"/* "$THEME_DIR/$THEME_NAME"
-    fi
+    echo_info "cp -a ./themes/\"$THEME_NAME\"/* \"$THEME_DIR/$THEME_NAME\""
+    cp -a ./themes/"$THEME_NAME"/* "$THEME_DIR/$THEME_NAME"
+  fi
 }
 
 function config_grub() {
-    echo_primary 'Enabling grub menu'
-    # remove default grub style if any
-    echo_info "sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub"
-    sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub
+  echo_primary 'Enabling grub menu'
+  # remove default grub style if any
+  echo_info "sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub"
+  sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub
 
-    echo_info "echo 'GRUB_TIMEOUT_STYLE=\"menu\"' >> /etc/default/grub"
-    echo 'GRUB_TIMEOUT_STYLE="menu"' >> /etc/default/grub
+  echo_info "echo 'GRUB_TIMEOUT_STYLE=\"menu\"' >> /etc/default/grub"
+  echo 'GRUB_TIMEOUT_STYLE="menu"' >>/etc/default/grub
 
-    #--------------------------------------------------
+  #--------------------------------------------------
 
-    echo_primary 'Setting grub timeout to 60 seconds'
-    # remove default timeout if any
-    echo_info "sed -i '/GRUB_TIMEOUT=/d' /etc/default/grub"
-    sed -i '/GRUB_TIMEOUT=/d' /etc/default/grub
+  echo_primary 'Setting grub timeout to 60 seconds'
+  # remove default timeout if any
+  echo_info "sed -i '/GRUB_TIMEOUT=/d' /etc/default/grub"
+  sed -i '/GRUB_TIMEOUT=/d' /etc/default/grub
 
-    echo_info "echo 'GRUB_TIMEOUT=\"60\"' >> /etc/default/grub"
-    echo 'GRUB_TIMEOUT="60"' >> /etc/default/grub
+  echo_info "echo 'GRUB_TIMEOUT=\"60\"' >> /etc/default/grub"
+  echo 'GRUB_TIMEOUT="60"' >>/etc/default/grub
 
-    #--------------------------------------------------
+  #--------------------------------------------------
 
-    echo_primary "Setting $THEME_NAME as default"
-    # remove theme if any
-    echo_info "sed -i '/GRUB_THEME=/d' /etc/default/grub"
-    sed -i '/GRUB_THEME=/d' /etc/default/grub
+  echo_primary "Setting $THEME_NAME as default"
+  # remove theme if any
+  echo_info "sed -i '/GRUB_THEME=/d' /etc/default/grub"
+  sed -i '/GRUB_THEME=/d' /etc/default/grub
 
-    echo_info "echo \"GRUB_THEME=\"$THEME_DIR/$THEME_NAME/theme.txt\"\" >> /etc/default/grub"
-    echo "GRUB_THEME=\"$THEME_DIR/$THEME_NAME/theme.txt\"" >> /etc/default/grub
-    
-    #--------------------------------------------------
+  echo_info "echo \"GRUB_THEME=\"$THEME_DIR/$THEME_NAME/theme.txt\"\" >> /etc/default/grub"
+  echo "GRUB_THEME=\"$THEME_DIR/$THEME_NAME/theme.txt\"" >>/etc/default/grub
 
-    echo_primary 'Setting grub graphics mode to auto'
-    # remove default timeout if any
-    echo_info "sed -i '/GRUB_GFXMODE=/d' /etc/default/grub"
-    sed -i '/GRUB_GFXMODE=/d' /etc/default/grub
+  #--------------------------------------------------
 
-    echo_info "echo 'GRUB_GFXMODE=\"auto\"' >> /etc/default/grub"
-    echo 'GRUB_GFXMODE="auto"' >> /etc/default/grub   
+  echo_primary 'Setting grub graphics mode to auto'
+  # remove default timeout if any
+  echo_info "sed -i '/GRUB_GFXMODE=/d' /etc/default/grub"
+  sed -i '/GRUB_GFXMODE=/d' /etc/default/grub
+
+  echo_info "echo 'GRUB_GFXMODE=\"auto\"' >> /etc/default/grub"
+  echo 'GRUB_GFXMODE="auto"' >>/etc/default/grub
 }
 
 function update_grub() {
-    # Update grub config
-    echo_primary 'Updating grub config...'
-    if [[ -x "$(command -v update-grub)" ]]; then
-        echo_info 'update-grub'
-        update-grub
+  # Update grub config
+  echo_primary 'Updating grub config...'
+  if [[ -x "$(command -v update-grub)" ]]; then
+    echo_info 'update-grub'
+    update-grub
 
-    elif [[ -x "$(command -v grub-mkconfig)" ]]; then
-        echo_info 'grub-mkconfig -o /boot/grub/grub.cfg'
-        grub-mkconfig -o /boot/grub/grub.cfg
+  elif [[ -x "$(command -v grub-mkconfig)" ]]; then
+    echo_info 'grub-mkconfig -o /boot/grub/grub.cfg'
+    grub-mkconfig -o /boot/grub/grub.cfg
 
-    elif [[ -x "$(command -v grub2-mkconfig)" ]]; then
-        if [[ -x "$(command -v zypper)" ]]; then
-            echo_info 'grub2-mkconfig -o /boot/grub2/grub.cfg'
-            grub2-mkconfig -o /boot/grub2/grub.cfg
+  elif [[ -x "$(command -v grub2-mkconfig)" ]]; then
+    if [[ -x "$(command -v zypper)" ]]; then
+      echo_info 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+      grub2-mkconfig -o /boot/grub2/grub.cfg
 
-        elif [[ -x "$(command -v dnf)" ]]; then
-            echo_info 'grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg'
-            grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-        fi
+    elif [[ -x "$(command -v dnf)" ]]; then
+      echo_info 'grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg'
+      grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
     fi
+  fi
 }
 
 function main() {
-    splash 'The Matrix awaits you...'
+  splash 'The Matrix awaits you...'
 
-    check_root
-    select_theme
+  check_root
+  select_theme
 
-    install_theme
+  install_theme
 
-    config_grub
-    update_grub
+  config_grub
+  update_grub
 
-    echo_success 'Boot Theme Update Successful!'
+  echo_success 'Boot Theme Update Successful!'
 }
 
 main
@@ -205,7 +215,6 @@ main
 sleep 10s
 
 ###        installation process begins here for grub-customizer (debian-based)
-
 
 echo Author: iemafzalhassan
 sleep 5s
@@ -221,14 +230,12 @@ sleep 1s
 echo Booooom.....!!!!
 sleep 2s
 
-
 echo "$(date)"
 sleep 2s
 echo "$(date)"
 sleep 2s
 echo wait...installation begins...here.!
 sleep 2s
-
 
 ##update checking...
 
@@ -243,13 +250,12 @@ sleep 2s
 echo cloning repo for installing grub customiser
 sleep 2s
 echo cloning repo from
-sleep 2s 
+sleep 2s
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 sleep 2s
 ##ckecking repo for update
-echo updating please wait..! 
+echo updating please wait..!
 sleep 2s
-
 
 sudo apt update
 sudo apt upgrade -y
@@ -259,8 +265,8 @@ sleep 2s
 
 echo grub-customiser installed
 sleep 2s
-echo checking lates build 
-sudo apt update 
+echo checking lates build
+sudo apt update
 sudo apt upgrade -y
 
 echo updated...
